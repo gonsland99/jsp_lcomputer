0dfceb8ec6169daaf5638e0da3eb00e3f2f4f9f0
kimbyunggon <Gon@DESKTOP-I5E3IS5>
2021-10-01 00:05:35 +0900

    테스트1

--- jsp_basic/src/main/java/com/mvc/controller/Controller.java
+++ jsp_basic/src/main/java/com/mvc/controller/Controller.java
@@ -19,6 +19,7 @@ import com.mvc.vo.User;
 public class Controller extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 	
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException{
 		doPost(request, response);
