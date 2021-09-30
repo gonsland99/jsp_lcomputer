99bc735534bc5253db613373f4f06b34b6c744c3
kimbyunggon <kimbyunggon@github.com>
2021-10-01 00:08:44 +0900

    git설정변경

--- jsp_basic/src/main/java/com/mvc/controller/Controller.java
+++ jsp_basic/src/main/java/com/mvc/controller/Controller.java
@@ -19,7 +19,6 @@ import com.mvc.vo.User;
 public class Controller extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 	
-	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException{
 		doPost(request, response);
